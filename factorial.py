5bea46b4f973ea9340ebb6c4ffa1ffe988fda107
Suchandra Paul <paulsuchandra79@gmail.com>
2026-05-09 13:10:11 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,12 @@
+num = int(input("Enter a number: "))
+
+if num < 0:
+    print("Factorial does not exist for negative numbers")
+
+else:
+    factorial = 1
+
+    for i in range(1, num + 1):
+        factorial *= i
+
+    print("Factorial of", num, "is", factorial)
